61e0cbe8a086fe24fbfb1a9a2a1afed5f52d9229
BryantLogan <bryant.logan1@gmail.com>
2023-02-03 13:01:20 -0700

    optional chaining

--- Mosh Beginner Course/hello-world/src/index.ts
+++ Mosh Beginner Course/hello-world/src/index.ts
@@ -1,8 +1,29 @@
-function greet(name: string | null | undefined) {
-    if (name)
-        console.log(name.toUpperCase());
-    else
-        console.log('Hola!')
+type Customer = {
+    birthday?: Date
 }
 
-greet(null);
+function getCustomer(id: number): Customer | null {
+    return id === 0 ? null : { birthday: new Date() };
+}
+
+// let customer = getCustomer(0);
+// if (customer !== null && customer !== undefined)
+//     console.log(customer.birthday);
+
+
+// Optional property access operator
+
+// let customer = getCustomer(0);
+// console.log(customer?.birthday);
+
+let customer = getCustomer(1);
+
+console.log(customer?.birthday?.getFullYear());
+
+
+// Optional element access operator
+// customers?.[0]
+
+// Optional call operator
+let log: any = null;
+log?.('a');
