85e2d9f8cfb8b6156ee1f3492828acaef00210e9
BryantLogan <bryant.logan1@gmail.com>
2023-02-03 11:22:58 -0700

    functions

--- Mosh Beginner Course/hello-world/src/index.ts
+++ Mosh Beginner Course/hello-world/src/index.ts
@@ -1,9 +1,20 @@
-// const small = 1;
-// const medium = 2;
-// const large = 3;
+// function calculateTax(income: number, taxYear: number): number {
+//     if (taxYear < 2022)
+//         return income * 1.2;
+//     return income * 1.3;
+// }
+// calculateTax(10_000, 2022);
 
-// PascalCase
-const enum Size { Small = 1, Medium, Large };
-let mySize: Size = Size.Medium;
+// function calculateTax(income: number, taxYear?: number): number {
+//     if ((taxYear || 2022) < 2022)
+//         return income * 1.2;
+//     return income * 1.3;
+// }
+// calculateTax(10_000);
 
-console.log(mySize)
+function calculateTax(income: number, taxYear= 2022): number {
+    if (taxYear  < 2022)
+        return income * 1.2;
+    return income * 1.3;
+}
+calculateTax(10_000);
