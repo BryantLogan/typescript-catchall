0dc5612fbe4faeafc81b26cc623af1410b6e1b36
BryantLogan <bryant.logan1@gmail.com>
2023-02-03 11:01:26 -0700

    arrays

--- Mosh Beginner Course/hello-world/src/index.ts
+++ Mosh Beginner Course/hello-world/src/index.ts
@@ -1,8 +1,3 @@
-let sales: number = 123_456_789;
-let course: string = 'TypeScript';
-let is_published: boolean = true;
-let level;
+let numbers: number[] = [];
 
-function render(document: any) {
-    console.log(document);
-}
+numbers.forEach(n => n )
