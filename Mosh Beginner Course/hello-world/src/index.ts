a4e07152d248870e1af490f865586c8756884554
BryantLogan <bryant.logan1@gmail.com>
2023-02-03 11:41:28 -0700

    objects

--- Mosh Beginner Course/hello-world/src/index.ts
+++ Mosh Beginner Course/hello-world/src/index.ts
@@ -1,20 +1,11 @@
-// function calculateTax(income: number, taxYear: number): number {
-//     if (taxYear < 2022)
-//         return income * 1.2;
-//     return income * 1.3;
-// }
-// calculateTax(10_000, 2022);
-
-// function calculateTax(income: number, taxYear?: number): number {
-//     if ((taxYear || 2022) < 2022)
-//         return income * 1.2;
-//     return income * 1.3;
-// }
-// calculateTax(10_000);
-
-function calculateTax(income: number, taxYear= 2022): number {
-    if (taxYear  < 2022)
-        return income * 1.2;
-    return income * 1.3;
-}
-calculateTax(10_000);
+let employee: { 
+    readonly id: number,
+    name: string,
+    retire: (date: Date) => void
+} = { 
+    id: 1, 
+    name: 'Bryant', 
+    retire: (date: Date) => {
+        console.log(date)
+    }
+};
